9adc781493c567b7b997c7be9abb0a08642813cd
tolf <140963545+tolfdog@users.noreply.github.com>
2025-05-30 08:45:45 -0700

    don't panic if a label is already signed

--- src/labels.rs
+++ src/labels.rs
@@ -1,10 +1,27 @@
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("already signed")]
+    AlreadySigned,
+
+    #[error("atrium_crypto: {0}")]
+    AtriumCrypto(#[from] atrium_crypto::Error),
+
+    #[error("serde_ipld_dagcbor: {0}")]
+    SerdeIpldDagcbor(#[from] serde_ipld_dagcbor::EncodeError<std::collections::TryReserveError>),
+
+    #[error("sqlx: {0}")]
+    Sqlx(#[from] sqlx::Error),
+}
+
 pub async fn emit(
     keypair: &atrium_crypto::keypair::Secp256k1Keypair,
     tx: &mut sqlx::PgTransaction<'_>,
     mut label: atrium_api::com::atproto::label::defs::Label,
     like_rkey: &str,
-) -> Result<i64, anyhow::Error> {
-    assert!(label.sig.is_none());
+) -> Result<i64, Error> {
+    if label.sig.is_some() {
+        return Err(Error::AlreadySigned);
+    }
 
     label.sig = Some(keypair.sign(&serde_ipld_dagcbor::to_vec(&label)?)?);
 
