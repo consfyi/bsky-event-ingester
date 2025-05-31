7eb79627a005dac2fe88e4f7dd440dfc43e3ede3
tolf <140963545+tolfdog@users.noreply.github.com>
2025-05-31 14:01:01 -0700

    normalize to nfc

--- src/bin/event_ingester.rs
+++ src/bin/event_ingester.rs
@@ -348,9 +348,11 @@ fn slugify(s: &str, langid: &icu_locale::LanguageIdentifier) -> String {
     static RE: std::sync::LazyLock<regex::Regex> =
         std::sync::LazyLock::new(|| regex::Regex::new(r"[^\p{L}\p{N}\s-]+").unwrap());
     const CASE_MAPPER: icu_casemap::CaseMapperBorrowed<'static> = icu_casemap::CaseMapper::new();
+    const NORMALIZER: icu_normalizer::ComposingNormalizerBorrowed<'static> =
+        icu_normalizer::ComposingNormalizer::new_nfc();
 
     CASE_MAPPER
-        .lowercase_to_string(&RE.replace_all(s, ""), langid)
+        .lowercase_to_string(&RE.replace_all(&NORMALIZER.normalize(s), ""), langid)
         .split_whitespace()
         .collect::<Vec<_>>()
         .join("-")
