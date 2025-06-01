7ee951246f7ba331910456013c59ad5eaa870f87
tolf <140963545+tolfdog@users.noreply.github.com>
2025-06-01 14:38:45 -0700

    move slug to separate module

--- src/bin/event_ingester.rs
+++ src/bin/event_ingester.rs
@@ -335,30 +335,6 @@ async fn fetch_old_events(
         }))
 }
 
-fn guess_language_for_region(
-    region: icu_locale::subtags::Region,
-) -> icu_locale::LanguageIdentifier {
-    const LOCALE_EXPANDER: icu_locale::LocaleExpander = icu_locale::LocaleExpander::new_common();
-    let mut langid = icu_locale::LanguageIdentifier::UNKNOWN;
-    langid.region = Some(region);
-    LOCALE_EXPANDER.maximize(&mut langid);
-    langid
-}
-
-fn slugify(s: &str, langid: &icu_locale::LanguageIdentifier) -> String {
-    static RE: std::sync::LazyLock<regex::Regex> =
-        std::sync::LazyLock::new(|| regex::Regex::new(r"[^\p{L}\p{N}\s-]+").unwrap());
-    const CASE_MAPPER: icu_casemap::CaseMapperBorrowed<'static> = icu_casemap::CaseMapper::new();
-    const NORMALIZER: icu_normalizer::ComposingNormalizerBorrowed<'static> =
-        icu_normalizer::ComposingNormalizer::new_nfkc();
-
-    CASE_MAPPER
-        .lowercase_to_string(&RE.replace_all(&NORMALIZER.normalize(s), ""), langid)
-        .split_whitespace()
-        .collect::<Vec<_>>()
-        .join("-")
-}
-
 async fn sync_labels(
     ics_url: &str,
     ui_endpoint: &str,
@@ -507,10 +483,10 @@ async fn sync_labels(
                     .as_ref()
                     .and_then(|g| g.country.as_ref())
                     .and_then(|c| c.parse().ok())
-                    .map(|region| guess_language_for_region(region))
+                    .map(|region| slug::guess_language_for_region(region))
                     .unwrap_or(icu_locale::LanguageIdentifier::UNKNOWN);
 
-                format!("{}-{}", slugify(&event.name, &langid), event.year)
+                format!("{}-{}", slug::slugify(&event.name, &langid), event.year)
             },
         );
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod base26;
 pub mod labels;
 pub mod roman;
+pub mod slug;
