612764406ffbdf393991f97ff164f4c2741e91a9
tolf <140963545+tolfdog@users.noreply.github.com>
2025-06-02 17:45:22 -0700

    more slugify stuff

--- src/slug.rs
+++ src/slug.rs
@@ -8,15 +8,36 @@ pub fn guess_language_for_region(
     langid
 }
 
-pub fn slugify(s: &str, langid: &icu_locale::LanguageIdentifier) -> String {
-    static RE: std::sync::LazyLock<regex::Regex> =
-        std::sync::LazyLock::new(|| regex::Regex::new(r"[^\p{L}\p{N}\s-]+").unwrap());
+fn map_cow<'a, T>(
+    input: std::borrow::Cow<'a, T>,
+    f: impl for<'b> Fn(&'b T) -> std::borrow::Cow<'b, T>,
+) -> std::borrow::Cow<'a, T>
+where
+    T: ToOwned + ?Sized,
+    T::Owned: AsRef<T> + 'a,
+{
+    match input {
+        std::borrow::Cow::Borrowed(b) => f(b),
+        std::borrow::Cow::Owned(o) => std::borrow::Cow::Owned(f(o.as_ref()).into_owned()),
+    }
+}
+
+pub fn to_lower<'a>(
+    s: &'a str,
+    langid: &icu_locale::LanguageIdentifier,
+) -> std::borrow::Cow<'a, str> {
     const CASE_MAPPER: icu_casemap::CaseMapperBorrowed<'static> = icu_casemap::CaseMapper::new();
     const NORMALIZER: icu_normalizer::ComposingNormalizerBorrowed<'static> =
         icu_normalizer::ComposingNormalizer::new_nfkc();
+    map_cow(NORMALIZER.normalize(s), |s| {
+        CASE_MAPPER.lowercase_to_string(s, langid)
+    })
+}
 
-    CASE_MAPPER
-        .lowercase_to_string(&RE.replace_all(&NORMALIZER.normalize(s), ""), langid)
+pub fn slugify(s: &str, langid: &icu_locale::LanguageIdentifier) -> String {
+    static RE: std::sync::LazyLock<regex::Regex> =
+        std::sync::LazyLock::new(|| regex::Regex::new(r"[^\p{L}\p{N}\s-]+").unwrap());
+    RE.replace_all(&to_lower(s, langid), "")
         .split_whitespace()
         .collect::<Vec<_>>()
         .join("-")
