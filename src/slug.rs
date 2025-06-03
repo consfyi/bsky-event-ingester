f8d29a919fc4c7e9a1c89d71fd7a89e5e3d00096
tolf <140963545+tolfdog@users.noreply.github.com>
2025-06-02 20:48:41 -0700

    add slugify_for_label

--- src/slug.rs
+++ src/slug.rs
@@ -1,3 +1,5 @@
+use crate::roman;
+
 pub fn guess_language_for_region(
     region: icu_locale::subtags::Region,
 ) -> icu_locale::LanguageIdentifier {
@@ -43,4 +45,97 @@ pub fn slugify(s: &str, langid: &icu_locale::LanguageIdentifier) -> String {
         .split_whitespace()
         .collect::<Vec<_>>()
         .join("-")
+}
+
+pub fn slugify_for_label(s: &str, langid: &icu_locale::LanguageIdentifier) -> String {
+    static NUMBERS_RE: std::sync::LazyLock<regex::Regex> =
+        std::sync::LazyLock::new(|| regex::Regex::new(r"\d+").unwrap());
+    static ALLOWED_CHARS_RE: std::sync::LazyLock<regex::Regex> =
+        std::sync::LazyLock::new(|| regex::Regex::new(r"[^a-z -]").unwrap());
+    ALLOWED_CHARS_RE
+        .replace_all(
+            &NUMBERS_RE.replace_all(
+                &deunicode::deunicode(&to_lower(s, langid)),
+                |caps: &regex::Captures| {
+                    format!(
+                        " {} ",
+                        roman::to_roman(caps[0].parse().unwrap()).to_lowercase()
+                    )
+                },
+            ),
+            "",
+        )
+        .split_whitespace()
+        .collect::<Vec<_>>()
+        .join("-")
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_slugify() {
+        assert_eq!(
+            slugify("Anthrocon", &icu_locale::langid!("en-US")),
+            "anthrocon"
+        );
+
+        assert_eq!(slugify("2Dance", &icu_locale::langid!("en-DE")), "2dance");
+
+        assert_eq!(
+            slugify("Futrołajki", &icu_locale::langid!("pl-PL")),
+            "futrołajki"
+        );
+
+        assert_eq!(
+            slugify("Örli Försztivál", &icu_locale::langid!("hu-HU")),
+            "örli-försztivál"
+        );
+
+        assert_eq!(
+            slugify("んなモフ", &icu_locale::langid!("ja-JP")),
+            "んなモフ"
+        );
+
+        assert_eq!(slugify("Fur-Eh!", &icu_locale::langid!("en-CA")), "fur-eh");
+    }
+
+    #[test]
+    fn test_slugify_for_label() {
+        assert_eq!(
+            slugify_for_label("Anthrocon", &icu_locale::langid!("en-US")),
+            "anthrocon"
+        );
+
+        assert_eq!(
+            slugify_for_label("2Dance", &icu_locale::langid!("en-DE")),
+            "ii-dance"
+        );
+
+        assert_eq!(
+            slugify_for_label("A2B", &icu_locale::langid!("en-US")),
+            "a-ii-b"
+        );
+
+        assert_eq!(
+            slugify_for_label("Futrołajki", &icu_locale::langid!("pl-PL")),
+            "futrolajki"
+        );
+
+        assert_eq!(
+            slugify_for_label("Örli Försztivál", &icu_locale::langid!("hu-HU")),
+            "orli-forsztival"
+        );
+
+        assert_eq!(
+            slugify_for_label("んなモフ", &icu_locale::langid!("ja-JP")),
+            "nnamohu"
+        );
+
+        assert_eq!(
+            slugify_for_label("Fur-Eh!", &icu_locale::langid!("en-CA")),
+            "fur-eh"
+        );
+    }
 }
